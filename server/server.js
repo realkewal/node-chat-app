fc334e521895d028ba7e7467e1f2839dab9a3620
realkewal <kew4l@yahoo.com.au>
2018-01-20 14:41:44 +1100

    Greet new user and alert user

--- server/server.js
+++ server/server.js
@@ -12,16 +12,37 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+    console.log('New user connected!');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined!',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
+
+
         console.log('Create Message', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
-    })
+
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+    });
 
     socket.on('disconnect', (socket) => {
         console.log('Client disconnected');
