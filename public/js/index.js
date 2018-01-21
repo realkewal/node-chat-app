a99c0f691a70fbb529b88c5828efb99b1b2ecee6
realkewal <kew4l@yahoo.com.au>
2018-01-21 14:32:34 +1100

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,20 +10,29 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     const formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('New Message received: ', message);
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    $('#messages').append(li);
+    const template = $('#message-template').html();
+    const html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
     const formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>')
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    const template = $('#location-message-template').html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    })
+    // var li = $('<li></li>');
+    // var a = $('<a target="_blank">My current location</a>')
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    $('#messages').append(html);
 });
 
 
